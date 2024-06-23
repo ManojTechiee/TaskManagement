da59da959e8145a56431279bb03c6beea428f75a
ManojTechiee <manojmahieie@gmail.com>
2024-06-24 01:38:34 +0530

    env file

--- TaskManager_BE/index.js
+++ TaskManager_BE/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -5,22 +6,20 @@ const tasksRouter = require('./routes/task');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const uri = process.env.MONGODB_URI;
 
 app.use(bodyParser.json());
 
-const uri = 'mongodb+srv://TaskManager:qexZRCvaVC6Wse0s@cluster0.k9kjljg.mongodb.net/'
-
 mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('MongoDB Atlas connected successfully');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB Atlas:', err.message);
-  });
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB Atlas connected successfully');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB Atlas:', err.message);
+});
 
 app.use('/tasks', tasksRouter);
 
